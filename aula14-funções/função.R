99c911a4730c5db3ed280cd4b42ecf8fec23e588
Arlyjr <arly.lcj@gmail.com>
2023-02-12 00:02:11 -0300

    Atualizado 1

---
+++ aula14-funções/função.R
@@ -0,0 +1,12 @@
+# O R já vem com várias funções instaladas e, com elas,
+#podemos executar muitas tarefas.
+#Criando a função
+par.impar <- function(3){ # nolint
+   if((num %% 2) == 0) {
+    return("Par")
+   }else {
+      return("Impar")
+   }
+#usando a função
+num = 3
+par.impar(num)
